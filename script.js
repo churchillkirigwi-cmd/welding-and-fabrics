8688c2acc4595ec1bed27f2ab290ea001c91b9c3
churchillkirigwi-cmd <churchillkirigwi@gmail.com>
2026-09-22 22:08:58 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector(".menu-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-if(menuToggle){
-menuToggle.addEventListener("click", ()=>{
-navLinks.classList.toggle("active");
-});
-}
